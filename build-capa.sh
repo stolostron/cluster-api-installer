bb6bcbe67416b6b96ea45f1023678722d424ae23
Marek Veber <mveber@redhat.com>
2024-12-12 17:57:23 +0100

    use kustomize build

--- build-capa.sh
+++ build-capa.sh
@@ -65,14 +65,6 @@ PRJ=cluster-api-provider-aws
 BRANCH="release-$OCP_VERSION"
 mkdir -p openshift
 
-SRC="openshift/$PRJ"
-if [ ! -d "$SRC" ] ; then
-    git clone https://github.com/openshift/"$PRJ" "$SRC"
-fi
-(cd $SRC; git checkout "$BRANCH"; git pull)
-SRC_CC="$SRC/openshift/infrastructure-components-openshift.yaml"
-[ -f "$SRC_CC" ] || SRC_CC="$SRC/openshift/infrastructure-components.yaml"
-
 if [ -z "$YQ" ] ; then
     echo "# using yq: $PWD/hack/tools/yq"
     YQ="$PWD/hack/tools/yq"
@@ -91,6 +83,15 @@ if [ ! -x "$KUSTOMIZE" ] ; then
     echo "use: make helm-capi"
     exit -1
 fi
+
+SRC="openshift/$PRJ"
+if [ ! -d "$SRC" ] ; then
+    git clone https://github.com/openshift/"$PRJ" "$SRC"
+fi
+SRC_CC="$PWD/openshift/$PRJ-components.yaml"
+(cd $SRC; git checkout "$BRANCH"; git pull)
+(cd $SRC; $KUSTOMIZE build config/default) > "$SRC_CC"
+
 
 echo "-------------------------"
 echo "PRJ=$PRJ BRANCH=$BRANCH"

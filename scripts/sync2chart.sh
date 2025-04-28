02f3121160768eee525d2636e035ff5542b4dac2
Marek Veber <mveber@redhat.com>
2025-04-28 14:46:14 +0200

    ACM-18750 - create a Jira task to review the created PR (#65)

--- scripts/sync2chart.sh
+++ scripts/sync2chart.sh
@@ -160,8 +160,12 @@ if [ "$SYNC2CHARTS" ] ;then
     generate_helm_template "$CHARTDIR" "$NEWCHART"
 
 
+    IS_UPDATED=false
+    if [ $(git diff --name-only "$CHARTDIR" $SRC_PROJECT_FILE|wc -l) -gt 0 ] ; then
+        IS_UPDATED=true
+    fi
+    echo "updated_$PROJECT=$IS_UPDATED"
     if [ -n "$GITHUB_OUTPUT" ] ; then
-        echo "using: GITHUB_OUTPUT=$GITHUB_OUTPUT NEWCHART=$NEWCHART"
         # when started under github workflow
         if [ $(git diff --name-only "$CHARTDIR"|wc -l) -gt 0 ] ; then
             echo "updated_$PROJECT=true" >> "$GITHUB_OUTPUT"

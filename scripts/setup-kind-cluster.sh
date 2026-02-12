5860a0dd7c07cc2b4317c5997dba51f6c11bbb23
Marek Veber <mveber@redhat.com>
2026-02-12 15:22:46 +0100

    prepare *-kind for all providers

--- scripts/setup-kind-cluster.sh
+++ scripts/setup-kind-cluster.sh
@@ -11,10 +11,31 @@ set -e
 #   KIND_CLUSTER_NAME - Name of the kind cluster (default: aso2)
 
 KIND_CLUSTER_NAME=${KIND_CLUSTER_NAME:-${1:-aso2}}
+HELM_INSTALL_TIMEOUT=${HELM_INSTALL_TIMEOUT:-10m}
+KIND_NODE_IMAGE=${KIND_NODE_IMAGE:-kindest/node:v1.31.14}
 
 if ! (kind get clusters 2>/dev/null|grep -q '^'"$KIND_CLUSTER_NAME"'$') ; then
-    kind create cluster --name "$KIND_CLUSTER_NAME" --image="kindest/node:v1.31.0"
+    SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+    if [ -z "$KIND_CFG_NAME" ] ; then
+        if [ -n "$DOCKER_SECRETS" ] ; then
+            KIND_CFG_NAME="$SCRIPT_DIR/kind-config-$KIND_CLUSTER_NAME.yaml"
+cat << EOF > "$KIND_CFG_NAME"
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  extraMounts:
+  - hostPath: $DOCKER_SECRETS
+    containerPath: /var/lib/kubelet/config.json
+EOF
+        fi
+    fi
+    KIND_OPTS="" 
+    if [ -f "$KIND_CFG_NAME" ] ; then
+        KIND_OPTS="$KIND_OPTS --config=$KIND_CFG_NAME"
+    fi
+    kind create cluster --name "$KIND_CLUSTER_NAME" --image="$KIND_NODE_IMAGE" $KIND_OPTS
     helm repo add jetstack https://charts.jetstack.io --force-update
     helm repo update
-    helm upgrade --install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --set crds.enabled=true --wait --timeout 5m
+    helm upgrade --install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --set crds.enabled=true --wait --timeout $HELM_INSTALL_TIMEOUT
 fi

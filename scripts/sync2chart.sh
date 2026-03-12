537ea783f53132273eab7c4bb7ac08cd832e25bc
Marek Veber <mveber@redhat.com>
2026-03-12 12:43:29 +0100

    Fix git diff for all suffixes while syncing manifests

--- scripts/sync2chart.sh
+++ scripts/sync2chart.sh
@@ -147,17 +147,16 @@ CHARTDIR="$PROJECT_ROOT/charts/$PROJECT"
 NEWCHART="$(realpath "$BUILTDIR")/new-chart.yml"
 
 if [ "$SYNC2CHARTS" ] ;then
+    IS_UPDATED=false
     for suffix in "" "-k8s" "-kind" ; do
         sync_chart_files "$BUILTDIR" "$CHARTDIR" "$suffix" 
         update_chart_versions "$CHARTDIR" "$CHART_VERSION" "$CHART_APP_VERSION" "$CHART_VALUES_IMAGE_TAG" "$suffix"
         generate_helm_template "$CHARTDIR" "$NEWCHART" "$suffix"
+        if [ $(git diff --name-only "$CHARTDIR$suffix" $SRC_PROJECT_FILE|wc -l) -gt 0 ] ; then
+            IS_UPDATED=true
+        fi
     done
 
-
-    IS_UPDATED=false
-    if [ $(git diff --name-only "$CHARTDIR" $SRC_PROJECT_FILE|wc -l) -gt 0 ] ; then
-        IS_UPDATED=true
-    fi
     echo "updated_$PROJECT=$IS_UPDATED"
     if [ -n "$GITHUB_OUTPUT" ] ; then
         # when started under github workflow

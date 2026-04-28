9e438aa0e6b72b66ddf7463a4a93fb4170501e17
Andrea Culot <95755271+andclt@users.noreply.github.com>
2026-04-28 14:59:46 +0200

    Fix cert-manager annotation in CAPA kind charts by removing hack.sh (#644)

--- config-kind/cluster-api-provider-aws/hack.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-SCRIPT_DIR=$(dirname "$0")
-${YQ} eval 'del(.vars[] | select(.name == "CERTIFICATE_NAME" or .name == "CERTIFICATE_NAMESPACE"))' "${SCRIPT_DIR}/default/kustomization.yaml" > "${SCRIPT_DIR}/default/kustomization.yaml.tmp"
-cp "${SCRIPT_DIR}/default/kustomization.yaml.tmp" "${SCRIPT_DIR}/default/kustomization.yaml"
